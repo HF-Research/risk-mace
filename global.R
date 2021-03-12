3924a228b985aa093c17197f23477dcd0c801889
matthew-phelps <ma.phelps@gmail.com>
2021-03-12 12:24:54 +0100

    Test commit after branching manuscript branch off

--- global.R
+++ global.R
@@ -6,6 +6,9 @@ library(ggplot2)
 library(r2d3)
 library(bsplus)
 # library(shinyBS),
+
+# LANGUAGE ----------------------------------------------------------------
+
 lang <- "en"
 if(lang=="dk"){
   source("ui/ui_dk.R", encoding = "UTF-8")
@@ -13,5 +16,8 @@ if(lang=="dk"){
   source("ui/ui_en.R", encoding = "UTF-8")
 
 }
+
+
+# LOAD OBJECTS ------------------------------------------------------------
 
 risk_table <- fread("data/risk_table_rounded.txt")
